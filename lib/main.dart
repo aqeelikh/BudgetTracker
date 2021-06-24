70e64b3cb7834977702c9244b49b6c318a945e40
Khalid Aqeeli <aqeelikh@gmail.com>
2021-06-24 21:43:18 +0300

    create budget tracker project

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+// To load .env file change main to an async function
+void main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Budget Tracker App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: BudgetScreen(),
+    );
+  }
+}
+
+class BudgetScreen extends StatefulWidget {
+  @override
+  _BudgetScreenState createState() => _BudgetScreenState();
+}
+
+class _BudgetScreenState extends State<BudgetScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
